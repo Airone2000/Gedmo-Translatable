20050a20a67a7bc0d473a8c648fef8cf72fc4986
ErwanGuillou <contact@erwanguillou.com>
2018-05-30 22:29:50 +0200

    Creating an article with two translatations And getting them using the repository

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -4,19 +4,54 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use Gedmo\Translator\Entity\Translation;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AppController extends AbstractController
 {
   /**
-   * @Route(path="/")
+   * @Route(path="/save-article")
    */
-  public function index()
+  public function saveArticle()
   {
+    $em = $this->getDoctrine()->getManager();
     
+    $article = new Article();
+    $article->setTitle('This is an english title');
+    $article->setContent('And this is an english content');
+    $article->setLocale('en_UK');
+    $em->persist($article);
+    $em->flush();
+    
+    $article->setTitle('Ceci est un titre français');
+    $article->setContent('Contenu en français');
+    $article->setLocale('fr_FR');
+    $em->flush();
     
     
     die('STOP');
+  }
+  
+  /**
+   * @Route(path="get-article/{id}")
+   * @param \App\Entity\Article $article
+   * @return \Symfony\Component\HttpFoundation\Response
+   */
+  public function getArticle(Article $article)
+  {
+    $em = $this->getDoctrine()->getRepository(\Gedmo\Translatable\Entity\Translation::class);
+    $translations = $em->findTranslations($article);
+    
+    echo '<pre>';
+    print_r($translations);
+    echo '</pre>';
+  
+  
+  
+    dump($article);
+    return new Response('<body></body>');
   }
 }
